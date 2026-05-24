ce55f7c392754b2f7bfc6829542f6fd0fa67b42f
notPlancha <31565873+notPlancha@users.noreply.github.com>
2026-05-24 19:40:13 +0200

    938 intuitive not work cause black always returns

--- problems/level 3/938/main.py
+++ problems/level 3/938/main.py
@@ -3,34 +3,49 @@ from dataclasses import dataclass
 class Deck:
   n_red: int
   n_black: int
+
+  def take_2_red(self) -> "Deck":
+    return Deck(self.n_red - 2, self.n_black)
+
+  def take_2_black(self) -> "Deck":
+    return Deck(self.n_red, self.n_black)
   
-  @property
-  def prob_red(self):
-    return self.n_red/(self.n_red + self.n_black)
-  @property
-  def prob_black(self):
-    return 1-self.prob_red
+  def take_1_each(self) -> "Deck":
+    return Deck(self.n_red, self.n_black - 1)
 
   @property
-  def take_red(self) -> "Deck":
-    return Deck(self.n_red - 1, self.n_black)
-  @property
-  def take_black(self) -> "Deck":
-    return Deck(self.n_red, self.n_black - 1)
-  
-  @property
-  def prob_both_red(self):
-    return self.prob_red * self.take_red.prob_red
-  @property
-  def prob_both_black(self):
-    return self.prob_black * self.take_black.prob_black
-  @property
-  def prob_one_or_other(self):
-    return (self.prob_red * self.take_red.prob_black + self.prob_black * self.take_black.prob_red)/2
-  
-  @property
   def all_red(self):
-    return self.n_black == 0 and self.n_red > 0
+    return self.n_black <= 0 and self.n_red > 0
   @property
   def all_black(self):
-    return self.n_red == 0 and self.n_black > 0
+    return self.n_red <= 0 and self.n_black > 0
+  
+# %% 
+# calculate tree and check which ones are all black
+out = {
+  "all_red_n" : 0,
+  "all_black_n": 0
+}
+
+def reset_out():
+  global out
+  out = {
+    "all_red_n" : 0,
+    "all_black_n": 0
+  }
+
+def simulate(deck: Deck):
+  if deck.all_red:
+    out["all_red_n"] += 1
+    return
+  elif deck.all_black:
+    out["all_black_n"] += 1
+    return
+  else:
+    simulate(deck.take_2_black())
+    simulate(deck.take_2_red())
+    simulate(deck.take_1_each())
+
+reset_out()
+simulate(Deck(2,2))
+print(out)
